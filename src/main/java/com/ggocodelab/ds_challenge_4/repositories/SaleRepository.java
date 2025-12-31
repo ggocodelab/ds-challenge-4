1df1db8abc79d2eec8df34f33e703ac02bcb048b
ggocodelab <brgillian@gmail.com>
2025-12-30 23:17:09 -0300

    feat(repository): add person creation repository

---
+++ src/main/java/com/ggocodelab/ds_challenge_4/repositories/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.ggocodelab.ds_challenge_4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<SaleRepository, Long>{
+
+}
---
+++ src/main/java/com/ggocodelab/ds_challenge_4/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.ggocodelab.ds_challenge_4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ggocodelab.ds_challenge_4.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
