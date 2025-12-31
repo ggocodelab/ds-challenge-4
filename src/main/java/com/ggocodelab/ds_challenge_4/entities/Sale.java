d4de40ba9a46f8e5570edf32aebea937372bd70d
ggocodelab <brgillian@gmail.com>
2025-12-30 22:08:56 -0300

    feat(sale): add sale entity

---
+++ src/main/java/com/ggocodelab/ds_challenge_4/entities/Sale.java
@@ -0,0 +1,97 @@
+package com.ggocodelab.ds_challenge_4.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_sale")
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer visited;
+	
+	private Integer deals;
+	
+	private Double amount;
+	
+	private LocalDate date;
+	
+	public Sale() {
+	}
+
+	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount, date, deals, id, visited);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(amount, other.amount) && Objects.equals(date, other.date)
+				&& Objects.equals(deals, other.deals) && Objects.equals(id, other.id)
+				&& Objects.equals(visited, other.visited);
+	}
+}
