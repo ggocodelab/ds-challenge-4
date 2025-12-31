8f5ee23ea7a31fece3acd267552e44ef4e2d2201
ggocodelab <brgillian@gmail.com>
2025-12-30 23:12:07 -0300

    chore(database): add initial seed data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_seller (name, email, phone) VALUES ('Anakin', 'anakin@galaxy.com', '11990000001');
+INSERT INTO tb_seller (name, email, phone) VALUES ('Logan', 'logan@xforce.com', '11990000002');
+INSERT INTO tb_seller (name, email, phone) VALUES ('Loki Odinson', 'loki@asgard.com', '11990000003');
+INSERT INTO tb_seller (name, email, phone) VALUES ('Padme', 'padme@naboo.com', '11990000004');
+INSERT INTO tb_seller (name, email, phone) VALUES ('Thor Odinson', 'thor@asgard.com', '11990000005');
+
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (50, 40, 19476.0, '2022-05-22', 3);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (50, 40, 19476.0, '2022-05-22', 3);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (42, 35, 20530.0, '2022-05-18', 5);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (38, 30, 21753.0, '2022-05-06', 3);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (60, 50, 25000.0, '2022-04-15', 1);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (55, 45, 28500.0, '2022-04-20', 1);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (48, 32, 30000.0, '2022-03-10', 2);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (52, 40, 27000.0, '2022-03-18', 2);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (65, 55, 35000.0, '2022-02-25', 4);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (70, 60, 32000.0, '2022-02-10', 4);
+INSERT INTO tb_sale (visited, deals, amount, date, seller_id) VALUES (58, 48, 29000.0, '2022-01-30', 5);
