522315ee103264a04fd4d2d898834cf19f32dd6c
ggocodelab <brgillian@gmail.com>
2025-12-31 23:19:05 -0300

    fix(repository): correct sale entity name

--- src/main/java/com/ggocodelab/ds_challenge_4/repositories/SaleRepository.java
+++ src/main/java/com/ggocodelab/ds_challenge_4/repositories/SaleRepository.java
@@ -2,6 +2,8 @@ package com.ggocodelab.ds_challenge_4.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SaleRepository extends JpaRepository<SaleRepository, Long>{
+import com.ggocodelab.ds_challenge_4.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
 
 }
