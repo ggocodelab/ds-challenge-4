45b76b054df9bd3dcc799da4571ed2ee0f563718
ggocodelab <brgillian@gmail.com>
2026-01-02 07:39:28 -0300

    chore(config): configure spring data web support for dto pagination

--- src/main/java/com/ggocodelab/ds_challenge_4/DsChallenge4Application.java
+++ src/main/java/com/ggocodelab/ds_challenge_4/DsChallenge4Application.java
@@ -2,10 +2,13 @@ package com.ggocodelab.ds_challenge_4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class DsChallenge4Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DsChallenge4Application.class, args);
 	}
